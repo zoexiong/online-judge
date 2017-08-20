93c334e7dae5cbb522d30844d16735e6e03492e3
zoexiong <xiongsy@uw.edu>
2017-08-19 23:15:27 -0700

    restructure files

--- week3/oj-server/routes/index.js
+++
@@ -1,9 +0,0 @@
-var express = require("express");
-var router = express.Router();
-var path = require('path');
-
-router.get("/", function(req, res){
-    res.sendFile("index.html", { root: path.join(__dirname, '../../public') })
-});
-
-module.exports = router;
